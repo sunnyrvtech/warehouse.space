c0d435d7d946ecd7c6441ffcf7deca2db8ac2b27
sunny <sunny_kumar@rvtechnologies.co.in>
2018-05-11 17:45:14 +0530

    11-05-2018

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,77 +10,38 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 24px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="container">
-     
-                <div class="top-right links">
-                        <a href="#">Link1</a>
-                        <a href="#">Link2</a>
-                        <a href="#">Link3</a>
+        <nav class="navbar navbar-expand-lg navbar-dark default-color fixed-top">
+            <div class="container">
+                <a class="ws-logo" href="#">
+                    <img src="{{ asset('/images/WSLogo.png') }}" alt="logo">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home
+                                <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Services</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
                 </div>
-
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Warehouse Dashboard
-                </div>
-
-                
             </div>
+        </nav>
+        <div class="container">
+            <h1>Warehouse Dashboard</h1>
         </div>
         <script src="{{ asset('js/app.js') }}"></script>
     </body>
