964c305bfbc0c1b62d1a42dff379e46619533609
sunny <sunny_kumar@rvtechnologies.co.in>
2018-05-11 10:56:08 +0530

    11-05-2018

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class InventoryController extends Controller
 {
     public function handleInventory(Request $request){
-         Log::info('Inventory:'.json_decode($requst->all()));
+         Log::info('Inventory:'.json_encode($request->all()));
     }
 }
