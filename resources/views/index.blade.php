c4b88f3bc9dce79a72e703efccb909d6a57bd299
sunny <sunny_kumar@rvtechnologies.co.in>
2018-05-12 15:40:38 +0530

    12-05-2018

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -40,8 +40,74 @@
                 </div>
             </div>
         </nav>
-        <div class="container">
-            <h1>Warehouse Dashboard</h1>
+        <div class="main-wrapper-content">
+            <div class="container">
+                <section>
+                    <h1 class="text-center">Warehouse Dashboard</h1>
+                    <!-- Nav tabs -->
+                    <div class="tabs-wrapper">
+                        <ul class="nav classic-tabs tabs-cyan" role="tablist">
+                            <li class="nav-item">
+                                <a class="nav-link active" data-toggle="tab" href="#panel_install" role="tab" aria-selected="false">Installation Instruction</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-toggle="tab" href="#panel_dev" role="tab" aria-selected="false">Developer Setting</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-toggle="tab" href="#panel_api" role="tab" aria-selected="false">Api Setting</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-toggle="tab" href="#panel_sync" role="tab" aria-selected="false">Resynchronize Products</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <!-- Tab panels -->
+                    <div class="tab-content card">
+                        <div class="tab-pane fade in show active" id="panel_install" role="tabpanel">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus
+                                reiciendis molestiae placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora, placeat ratione
+                                porro voluptate odit minima.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus
+                                reiciendis molestiae placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora, placeat ratione
+                                porro voluptate odit minima.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus
+                                reiciendis molestiae placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora, placeat ratione
+                                porro voluptate odit minima.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus
+                                reiciendis molestiae placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora, placeat ratione
+                                porro voluptate odit minima.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus
+                                reiciendis molestiae placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora, placeat ratione
+                                porro voluptate odit minima.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus
+                                reiciendis molestiae placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora, placeat ratione
+                                porro voluptate odit minima.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus
+                                reiciendis molestiae placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora, placeat ratione
+                                porro voluptate odit minima.</p>
+                        </div>
+                        <div class="tab-pane fade in" id="panel_dev" role="tabpanel">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus
+                                reiciendis molestiae placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora, placeat ratione
+                                porro voluptate odit minima.</p>
+                        </div>
+                        <div class="tab-pane fade" id="panel_api" role="tabpanel">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus
+                                reiciendis molestiae placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora, placeat ratione
+                                porro voluptate odit minima.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus
+                                reiciendis molestiae placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora, placeat ratione
+                                porro voluptate odit minima.</p>
+
+                        </div>
+                        <div class="tab-pane fade" id="panel_sync" role="tabpanel">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil odit magnam minima, soluta doloribus
+                                reiciendis molestiae placeat unde eos molestias. Quisquam aperiam, pariatur. Tempora, placeat ratione
+                                porro voluptate odit minima.</p>
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
         <script src="{{ asset('js/app.js') }}"></script>
     </body>
