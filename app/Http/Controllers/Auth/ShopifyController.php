75d8c41e9c8812e3ad94de2695f37f1d7e70238f
sunny <sunny_kumar@rvtechnologies.co.in>
2018-05-14 12:48:46 +0530

    14-05-2018

--- app/Http/Controllers/Auth/ShopifyController.php
+++ app/Http/Controllers/Auth/ShopifyController.php
@@ -25,6 +25,7 @@ class ShopifyController extends Controller {
         $user = User::Where('shop_url', $shopUrl);
         if ($user->count() > 0) {
             if (!auth()->check()) {
+                die;
                 return redirect()->route('authenticate', $shopUrl);
             }
             return redirect()->to('/dashboard');
