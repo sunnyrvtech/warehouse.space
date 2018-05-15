9c72cf74bde87890cee251425209124e00e304f0
sunny <sunny_kumar@rvtechnologies.co.in>
2018-05-15 18:23:18 +0530

    15-05-2018

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -21,8 +21,39 @@ class ProductController extends Controller {
         $this->middleware(function ($request, $next) {
             $this->_user = auth()->user();
             $user = $this->_user;
-            if (isset($user->get_dev_setting)) {
-                $this->_accountKey = $user->get_dev_setting->account_key;
+//            if (isset($user->get_dev_setting)) {
+//                $this->_accountKey = $user->get_dev_setting->account_key;
+//                $this->_warehouseNumber = $user->get_dev_setting->warehouse_number;
+//                $debug = true;
+//                $wsdl = $user->get_dev_setting->wsdl_url;
+//                try {
+//                    $this->_client = new $soapClient($wsdl, array(
+//                        'cache_wsdl' => $debug ? WSDL_CACHE_NONE : WSDL_CACHE_MEMORY,
+//                        'trace' => true,
+//                        'exceptions' => true,
+//                        'soap_version' => SOAP_1_1
+//                            )
+//                    );
+//                } catch (SoapFault $fault) {
+//                    WarehouseSpace_Warehouse_Helper_Data::log('Soap client error: ' . $fault->getMessage());
+//                    //throw new Mage_Core_Exception('We could not connect to Warehouse.Space');
+//                }
+//            }
+            return $next($request);
+        });
+    }
+
+    public function handleProducts(Request $request, $slug) {
+        Log::info('Products ' . $slug . ':' . json_encode($request->all()));
+    }
+
+    public function synchronizeProducts(Request $request) {
+        $user = $this->_user;
+//        $client = $this->_client;
+        
+        
+        
+        $this->_accountKey = $user->get_dev_setting->account_key;
                 $this->_warehouseNumber = $user->get_dev_setting->warehouse_number;
                 $debug = true;
                 $wsdl = $user->get_dev_setting->wsdl_url;
@@ -38,18 +69,9 @@ class ProductController extends Controller {
                     WarehouseSpace_Warehouse_Helper_Data::log('Soap client error: ' . $fault->getMessage());
                     //throw new Mage_Core_Exception('We could not connect to Warehouse.Space');
                 }
-            }
-            return $next($request);
-        });
-    }
-
-    public function handleProducts(Request $request, $slug) {
-        Log::info('Products ' . $slug . ':' . json_encode($request->all()));
-    }
-
-    public function synchronizeProducts(Request $request) {
-        $user = $this->_user;
-        $client = $this->_client;
+        
+        
+        
 
 //        $parameters = (object) array();
 //        $parameters->article = 214;
@@ -72,7 +94,7 @@ class ProductController extends Controller {
 //        $parameters->warehouse = $this->_warehouseNumber;
 //        $parameters->AccountKey = $this->_accountKey;
         
-        dd($client->__getFunctions());
+        dd($this->_client);
         
 //        $obj = $client->material($parameters);
 //        dd($obj);
