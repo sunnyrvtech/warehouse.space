a44ced5fd0a73986c6d21411c66f37f25521889b
sunny <sunny_kumar@rvtechnologies.co.in>
2018-05-17 18:59:43 +0530

    17-05-2018

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -86,8 +86,8 @@ class ProductController extends Controller {
                 $final_product_array = (object) array();
                 $final_product_array->ArticlesList = $product_array;
 
-//                $result = $client->MaterialBulk($final_product_array);
-                Log::info('Products ' . $slug . '(id):' . json_encode($final_product_array));
+                $result = $client->MaterialBulk($final_product_array);
+                Log::info('Products ' . $slug . '(id):' . $request->get('id'));
                 exit();
             }
             Log::info('Products ' . $slug . 'not saved account setting yet !');
