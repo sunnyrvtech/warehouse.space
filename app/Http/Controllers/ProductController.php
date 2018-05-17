394b3891a93862cd64497cbeb76d7ea257a90c7d
sunny <sunny_kumar@rvtechnologies.co.in>
2018-05-17 17:01:50 +0530

    17-05-2018

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -48,7 +48,7 @@ class ProductController extends Controller {
 
     public function handleProducts(Request $request, $slug) {
  
-         Log::info('Products ' . $slug . '(id):' .json_encode($request->all()));
+         Log::info('Products ' . $slug . '(id):' .json_encode($request->headers->all()));
         
     }
 
