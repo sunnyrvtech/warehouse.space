f5d37f220ec3935b23fe95a36eed000c9dfdf259
sunny <sunny_kumar@rvtechnologies.co.in>
2018-05-24 18:06:29 +0530

    24-05-2018

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -40,7 +40,7 @@ class OrderController extends Controller {
         $user = User::Where('shop_url', $shopUrl)->first();
         if ($client != null) {
             if (isset($user->get_dev_setting)) {
-                if ($slug == "create" || $slug == "update") {
+                if ($slug == "create") {
                     $result = $this->createOrder($request, $user);
                     Log::info($shopUrl.' Order ' . $slug . json_encode($result));
                     exit();
@@ -131,7 +131,7 @@ class OrderController extends Controller {
         $order_array->ShortCode = "";
         $order_array->Warehouse = $user->get_dev_setting->warehouse_number;
         $order_array->AccountKey = $user->get_dev_setting->account_key;
-         Log::info(' Order update' . json_encode($order_array));
+        Log::info(' Order update' . json_encode($order_array));
         $result = $client->OrderDetail($order_array);
         return $result;
     }
