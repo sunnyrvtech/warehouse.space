addc7f582fd30dcfd783d2fd6b1818e1253076ff
sunny <sunny_kumar@rvtechnologies.co.in>
2018-05-30 10:49:12 +0530

    30-05-2018

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -188,7 +188,7 @@ class OrderController extends Controller {
 
             foreach ($result as $value) {
                 
-                    $orders = $shopify->call(['URL' => 'orders/403676758068.json', 'METHOD' => 'GET']);
+                    $orders = $shopify->call(['URL' => 'orders/'. $value->InvNumber . '.json', 'METHOD' => 'GET']);
                       dd($orders);  
 //                $shopify$value
             }
