7c89e4e9a9c9812a1f8d5fa0238ee3eafc274b87
sunny <sunny_kumar@rvtechnologies.co.in>
2018-06-01 18:01:49 +0530

    01-06-2018

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -276,7 +276,7 @@ class OrderController extends Controller {
                             if ($article->ProductID == $orders->order->line_items[$key]->variant_id) {
                                 $item_ids_array[$key] = $orders->order->line_items[$key]->id;
                             } elseif ($article->ProductID == $orders->order->line_items[$key + 1]->variant_id) {
-                                $item_ids_array[$key] = $orders->order->line_items[$key]->id;
+                                $item_ids_array[$key] = $orders->order->line_items[$key+1]->id;
                             }
                         }
                         echo count($warehouse_shipment);
