3be60a0b54c16ed6f950dc10c9ec3a2f8da0c304
sunny <sunny_kumar@rvtechnologies.co.in>
2018-06-01 19:56:04 +0530

    01-06-2018

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -255,7 +255,7 @@ class OrderController extends Controller {
                 }
                 //dd($orders);
 
-                if ($warehouse_order->OrderStatus == 4 && $orders->order->fulfillment_status == 'partial' && isset($warehouse_order->Shipments->ShipmentDetail)) {
+                if ($warehouse_order->OrderStatus == 4 && $orders->order->fulfillment_status == null && isset($warehouse_order->Shipments->ShipmentDetail)) {
                     $warehouse_shipment = $warehouse_order->Shipments->ShipmentDetail;
                     if (count($warehouse_order->Shipments->ShipmentDetail) == 1) {
                         $shipment_array[0] = $warehouse_order->Shipments->ShipmentDetail;
