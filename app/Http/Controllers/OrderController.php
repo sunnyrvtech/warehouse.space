686cfba2ad2f19829313a55c7a3c79396fb00377
sunny <sunny_kumar@rvtechnologies.co.in>
2018-06-01 19:34:27 +0530

    01-06-2018

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -282,7 +282,7 @@ class OrderController extends Controller {
                         // echo count($warehouse_shipment);
                         //dd($item_ids_array);
                         //try {
-                            $shopify_result = $shopify->call(['URL' => 'orders/' . $id . '/fulfillments.json', 'METHOD' => 'POST', "DATA" => ["fulfillment" => array("location_id" => null, "tracking_number" => $shipment->TrackingNumber, "line_items" => $item_ids_array)]]);
+                            $shopify_result = (['URL' => 'orders/' . $id . '/fulfillments.json', 'METHOD' => 'POST', "DATA" => ["fulfillment" => array("location_id" => null, "tracking_number" => $shipment->TrackingNumber, "line_items" => $item_ids_array)]]);
                        // } catch (\Exception $e) {
                            // Log::info('Order status update error ' . $id . $e->getMessage());
                          //   return json_encode(array('success' => false));
