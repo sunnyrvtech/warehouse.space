ebab504d2acbce58eb68ca7f7179990e7918057c
sunny <sunny_kumar@rvtechnologies.co.in>
2018-06-04 16:05:34 +0530

    04-06-2018

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -171,7 +171,7 @@ class OrderController extends Controller {
 //        echo htmlentities($client->__getLastRequest());
         echo "<pre>";
 //        print_r($request_array);
-        print_r($warehouse_order);
+        dd($warehouse_order);
         if (isset($warehouse_order->GetOrderShipmentInfoResult->OrderShipmentInfo)) {
             $shopify = App::makeWith('ShopifyAPI', ['API_KEY' => env('SHOPIFY_APP_KEY'), 'API_SECRET' => env('SHOPIFY_APP_SECRET'), 'SHOP_DOMAIN' => $user->shop_url, 'ACCESS_TOKEN' => $user->access_token]);
 
