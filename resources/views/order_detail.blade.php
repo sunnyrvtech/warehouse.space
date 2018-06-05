6274e50a0853f8d0bdce634d46171717cc417bb7
sunny <sunny_kumar@rvtechnologies.co.in>
2018-06-05 11:48:30 +0530

    05-06-2018

--- resources/views/order_detail.blade.php
+++ resources/views/order_detail.blade.php
@@ -55,7 +55,7 @@
                                             <div class="col-md-3">
                                                 @if($value->YoutubeUrl !='')
                                                 <!--https://www.youtube.com/embed/ddzU-rkzKF0-->
-                                                <iframe width="200" height="140" src="{{ $value->YoutubeUrl }}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen="allowfullscreen"></iframe>
+                                                <iframe width="200" height="140" src="{{ $value->YoutubeUrl }}"></iframe>
                                                 @endif
                                             </div>
                                         </div>
