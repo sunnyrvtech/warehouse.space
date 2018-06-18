36d1b32034a1b942d92e72c1a7aebd02d2024cdd
sunny <sunny_kumar@rvtechnologies.co.in>
2018-06-18 19:43:26 +0530

    18-06-2018

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,6 +13,15 @@
                 <!--<p>We pick, pack and dispatch products with next day delivery.</p>-->
             </div>
         </div>
+        <div class="card-header bg-info text-white">Requirements</div>
+        <div class="card-body">
+            <div class="instruction-content">
+                <p>To help us pick, pack and dispatch the correct products we need your help.Every product in our warehouse has a barcode. If the product doesn’t have one when you ship it to one of our warehouses, then we will add a barcode to the product packaging.This barcode is what we scan to confirm we have the correct product your customer has ordered.</p>
+                <p>To help us with this process we need you to ensure that every product you have in your store has a Barcode value defined that matches the barcode on the product. The barcode must be unique per product, including variants.We are unable to receive a product into our warehouse that you have not defined its barcode value.</p>
+                <p>Thanks for your assistance in helping us, ship your products perfectly every time.
+                    to the left of this text, please take a screen shot of the barcode input section of the product page in shopify.</p>
+            </div>
+        </div>
         <div class="card-footer">Support:-</div>
     </div>
 </section>
