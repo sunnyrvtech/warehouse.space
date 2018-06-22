58cc9bbd760530f7e99d69839aa9bff1cd11839f
sunny <sunny_kumar@rvtechnologies.co.in>
2018-06-22 16:29:32 +0530

    22-06-2018

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -168,6 +168,7 @@ class ProductController extends Controller {
                     $root->appendChild($items);
                     $i++;
                 }
+                $i++;
             }
             }
 //            echo $dom->getElementsByTagName('MaterialArticle')->length;
