0404181933874f88169bb83c587f70cad7ff5950
sunny <sunny_kumar@rvtechnologies.co.in>
2018-06-22 20:47:44 +0530

    22-06-2018

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -102,7 +102,7 @@ class OrderController extends Controller {
             );
         }
         
-            Log::info(' Order update' . $request->get('shipping_lines')[0]->price);
+            Log::info(' Order update' . $request->get('shipping_lines')[0]['price']);
 die;
         $order_array->ArticlesList = $article_array;
         $order_array->InvNumber = $request->get('id');
