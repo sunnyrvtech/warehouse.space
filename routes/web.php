9e818b1f7f60516569168374a27fed476c716a2b
sunny <sunny_kumar@rvtechnologies.co.in>
2018-06-28 18:09:49 +0530

    28-06-2018

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  GET');
+
 /*
   |--------------------------------------------------------------------------
   | Web Routes
