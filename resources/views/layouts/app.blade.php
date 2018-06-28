5380a789edad3cd5ca1be1a503f275e8127cd9a9
sunny <sunny_kumar@rvtechnologies.co.in>
2018-06-28 18:52:22 +0530

    28-06-2018

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Warehouse Space</title>
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token()}}">
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
