1393e500bd87741f83efa9d78dbe490d39e0a363
sunny <sunny_kumar@rvtechnologies.co.in>
2018-07-26 18:31:55 +0530

    26-07-2018

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -180,9 +180,9 @@ class OrderController extends Controller {
 
         $warehouse_order = $client->GetOrderShipmentInfo($request_array);
 //        echo htmlentities($client->__getLastRequest());
-        echo "<pre>";
-        print_r($request_array);
-        dd($warehouse_order);
+//        echo "<pre>";
+//        print_r($request_array);
+//        dd($warehouse_order);
         if (isset($warehouse_order->GetOrderShipmentInfoResult->OrderShipmentInfo)) {
             $warehouse_shipment = $warehouse_order->GetOrderShipmentInfoResult->OrderShipmentInfo->Shipments;
             $shopify = App::makeWith('ShopifyAPI', ['API_KEY' => env('SHOPIFY_APP_KEY'), 'API_SECRET' => env('SHOPIFY_APP_SECRET'), 'SHOP_DOMAIN' => $user->shop_url, 'ACCESS_TOKEN' => $user->access_token]);
@@ -354,7 +354,7 @@ class OrderController extends Controller {
                         }
                         $item_ids_array = array_values($item_ids_array);
                         // echo count($warehouse_shipment);
-                        //dd($item_ids_array);
+                        dd($item_ids_array);
                         try {
                             $shopify_result = $shopify->call(['URL' => 'orders/' . $id . '/fulfillments.json', 'METHOD' => 'POST', "DATA" => ["fulfillment" => array("location_id" => $locations->locations[0]->id, "tracking_number" => $shipment->TrackingNumber, "line_items" => $item_ids_array)]]);
                         } catch (\Exception $e) {
