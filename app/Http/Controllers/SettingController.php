37ae59cbd8f2b990420bf6a4baafdc35dc771a6e
sunny <sunny_kumar@rvtechnologies.co.in>
2018-09-29 14:41:23 +0530

    29-09-2018

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -57,6 +57,13 @@ class SettingController extends Controller {
             'track_order' => 'required',
             'stock' => 'required',
         ]);
+        $user  = auth()->user();
+        
+          $sh = App::makeWith('ShopifyAPI', ['API_KEY' => env('SHOPIFY_APP_KEY'), 'API_SECRET' => env('SHOPIFY_APP_SECRET'), 'SHOP_DOMAIN' => $user->shop_url, 'ACCESS_TOKEN' => $user->access_token]);
+
+            $recurring = $sh->call(['URL' => 'recurring_application_charges.json', 'METHOD' => 'POST', "DATA" => ["recurring_application_charge" => array("name" => "Free", "price" => 0.00, "return_url" => $redirect_url)]]);
+            
+        dd($recurring);
 
         $data['user_id'] = auth()->id();
         if ($api_data = ApiSetting::Where('user_id', auth()->id())->first()) {
