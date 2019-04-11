47fccdb9c12fa9b80cd015014e992b3c62b1f28f
sunny <sunny_kumar@rvtechnologies.co.in>
2019-04-11 13:38:35 +0530

    location api commit

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -199,6 +199,7 @@ class SettingController extends Controller {
     }
 
     public function getFulfillmentLocations($id, $token) {
+        die('hello');
         $user = DeveloperSetting::Where(['store_id' => $id, 'warehouse_token' => $token])->first();
         if (isset($user->get_user)) {
             $sh = App::makeWith('ShopifyAPI', ['API_KEY' => env('SHOPIFY_APP_KEY'), 'API_SECRET' => env('SHOPIFY_APP_SECRET'), 'SHOP_DOMAIN' => $user->shop_url, 'ACCESS_TOKEN' => $user->access_token]);
