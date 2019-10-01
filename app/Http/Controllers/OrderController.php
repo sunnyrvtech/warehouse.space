f3395c94619119bc5857c4b534d16d22f50b9722
sunny <sunny_kumar@rvtechnologies.co.in>
2019-10-01 12:13:03 +0530

    new chnage s1

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -336,11 +336,11 @@ class OrderController extends Controller {
             $request_array->ListInvNumbers = array($id);
             $warehouse_order = $client->GetOrderShipmentInfo($request_array);
             $shopify = App::makeWith('ShopifyAPI', ['API_KEY' => env('SHOPIFY_APP_KEY'), 'API_SECRET' => env('SHOPIFY_APP_SECRET'), 'SHOP_DOMAIN' => $user->get_user->shop_url, 'ACCESS_TOKEN' => $user->get_user->access_token]);
-           Log::info('Order api response'. json_encode($warehouse_order));
-           echo "<pre>";
-           print_r($request_array);
-           print_r($warehouse_order);
-           die;
+           // Log::info('Order api response'. json_encode($warehouse_order));
+           // echo "<pre>";
+           // print_r($request_array);
+           // print_r($warehouse_order);
+           // die;
 
             if (isset($warehouse_order->GetOrderShipmentInfoResult->OrderShipmentInfo)) {
                 $warehouse_order = $warehouse_order->GetOrderShipmentInfoResult->OrderShipmentInfo;
@@ -350,7 +350,8 @@ class OrderController extends Controller {
                     return json_encode(array('success' => false, 'message' => $e->getMessage()));
                 }
 
-                //dd($orders);
+                dd($orders);
+                 die;
 
                 if ($warehouse_order->OrderStatus == 4 && $orders->order->fulfillment_status == null && isset($warehouse_order->Shipments->ShipmentDetail)) {
                     $warehouse_shipment = $warehouse_order->Shipments->ShipmentDetail;
