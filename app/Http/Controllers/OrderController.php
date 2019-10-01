a55515bd2d148ff0565bcc610e334642d206dbc3
sunny <sunny_kumar@rvtechnologies.co.in>
2019-10-01 12:42:02 +0530

    new chnage s11

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -358,9 +358,9 @@ class OrderController extends Controller {
                         $shipment_array[0] = $warehouse_order->Shipments->ShipmentDetail;
                         $warehouse_shipment = $shipment_array;
                     }
-                    //echo "<pre>";
-                    //print_r($warehouse_shipment);
-
+                    echo "<pre>";
+                    print_r($warehouse_shipment);
+                    die;
                     foreach ($warehouse_shipment as $shipment) {
                         $articles = $shipment->Articles->Article;
                         if (count($shipment->Articles->Article) == 1) {
