1ce5d50306683c24ddc0c0989c5d42fcdee4dbd6
sunny <sunny_kumar@rvtechnologies.co.in>
2019-10-07 19:52:26 +0530

    new chnage 7oct32

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -105,10 +105,10 @@ class ProductController extends Controller {
                         $item_array->ProductID = $item_value->id;
                         $item_array->Article = $item_value->sku;
                         $item_array->Title = htmlspecialchars($item_value->title);
-                        if($item_value->barcode !=""){
-		                    $item_array->Barcode = $item_value->barcode;
+                        if($item_value->barcode ==null){
+		                    $item_array->Barcode = null;
 		                }else{
-                            $item_array->Barcode ="";
+                            $item_array->Barcode =$item_value->barcode;
 		                }
                         $item_array->Description = htmlspecialchars(strip_tags($request->body_html));
 //                    $item_array->ErpTimeStamp = date('Y-m-d-H:i');
