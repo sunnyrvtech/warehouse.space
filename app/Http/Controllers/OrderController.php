9ab82c4a86101a9343a35f0ce75062db61070839
sunny <sunny_kumar@rvtechnologies.co.in>
2019-10-21 13:22:07 +0530

    new chnage s2

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -361,9 +361,9 @@ class OrderController extends Controller {
                         $shipment_array[0] = $warehouse_order->Shipments->ShipmentDetail;
                         $warehouse_shipment = $shipment_array;
                     }
-                    echo "<pre>";
-                    print_r($warehouse_shipment);
-                    die;
+                    // echo "<pre>";
+                    // print_r($warehouse_shipment);
+                    // die;
 
                     foreach ($warehouse_shipment as $shipment) {
                         $articles = $shipment->Articles->Article;
@@ -422,7 +422,7 @@ class OrderController extends Controller {
                             Log::info('Order status update error ' . $id . $e->getMessage());
                             return json_encode(array('success' => false, 'message' => $e->getMessage()));
                         }
-//                            dd($shopify_result);
+                           dd($shopify_result);
                     }
                     return response()->json(['success' => true], 200);
                 } else if ($warehouse_order->OrderStatus == 4 && $orders->order->fulfillment_status != null && isset($warehouse_order->Shipments->ShipmentDetail)) {
