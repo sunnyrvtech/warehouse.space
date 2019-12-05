316aee509d9530acd61638764e74df0d0ebe4f00
sunny <sunny_kumar@rvtechnologies.co.in>
2019-12-05 18:34:52 +0530

    new chnage s6

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -161,7 +161,8 @@ class OrderController extends Controller {
         $order_array->Warehouse = $user->get_dev_setting->warehouse_number;
         $order_array->AccountKey = $user->get_dev_setting->account_key . '|' . $user->get_dev_setting->store_id;
         $result = $client->OrderDetail($order_array);
-        Log::info('Create order details:'.json_encode($result));
+        //Log::info('Create order details:'.json_encode($result));
+        Log::info('Create order details:'. print_r($result, true));
         //Log::info(' Order update' . $client->__getLastRequest());
 //        Log::info(' Order update' . $client->__getLastResponse());
         return $result;
