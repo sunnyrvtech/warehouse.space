9dacff464d2006fcbb27dbc7e8db1e6c8a98f8b3
sunny <sunny_kumar@rvtechnologies.co.in>
2019-12-05 18:05:18 +0530

    new chnage s2

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -199,9 +199,9 @@ class OrderController extends Controller {
 
         $warehouse_order = $client->GetOrderShipmentInfo($request_array);
 //        echo htmlentities($client->__getLastRequest());
-//        echo "<pre>";
-//        print_r($request_array);
-//        dd($warehouse_order);
+       echo "<pre>";
+       print_r($request_array);
+       dd($warehouse_order);
         if (isset($warehouse_order->GetOrderShipmentInfoResult->OrderShipmentInfo)) {
             if (isset($warehouse_order->GetOrderShipmentInfoResult->OrderShipmentInfo->Shipments) && count((array) $warehouse_order->GetOrderShipmentInfoResult->OrderShipmentInfo->Shipments)) {
                 $warehouse_shipment = $warehouse_order->GetOrderShipmentInfoResult->OrderShipmentInfo->Shipments;
@@ -339,11 +339,11 @@ class OrderController extends Controller {
             $request_array->ListInvNumbers = array($id);
             $warehouse_order = $client->GetOrderShipmentInfo($request_array);
             $shopify = App::makeWith('ShopifyAPI', ['API_KEY' => env('SHOPIFY_APP_KEY'), 'API_SECRET' => env('SHOPIFY_APP_SECRET'), 'SHOP_DOMAIN' => $user->get_user->shop_url, 'ACCESS_TOKEN' => $user->get_user->access_token]);
-           Log::info('Order api response'. json_encode($warehouse_order));
-           echo "<pre>";
-           print_r($request_array);
-           print_r($warehouse_order);
-           die;
+           // Log::info('Order api response'. json_encode($warehouse_order));
+           // echo "<pre>";
+           // print_r($request_array);
+           // print_r($warehouse_order);
+           // die;
 
             if (isset($warehouse_order->GetOrderShipmentInfoResult->OrderShipmentInfo)) {
                 $warehouse_order = $warehouse_order->GetOrderShipmentInfoResult->OrderShipmentInfo;
