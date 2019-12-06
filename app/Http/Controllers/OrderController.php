0bb036226855e81e9256b215c0ceef766f3d9d33
sunny <sunny_kumar@rvtechnologies.co.in>
2019-12-06 15:57:31 +0530

    new chnage s9

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -340,10 +340,10 @@ class OrderController extends Controller {
             $warehouse_order = $client->GetOrderShipmentInfo($request_array);
             $shopify = App::makeWith('ShopifyAPI', ['API_KEY' => env('SHOPIFY_APP_KEY'), 'API_SECRET' => env('SHOPIFY_APP_SECRET'), 'SHOP_DOMAIN' => $user->get_user->shop_url, 'ACCESS_TOKEN' => $user->get_user->access_token]);
 //            Log::info('Order api response'. json_encode($warehouse_order));
-//            echo "<pre>";
-//            print_r($request_array);
-//            print_r($warehouse_order);
-//            die;
+           echo "<pre>";
+           print_r($request_array);
+           print_r($warehouse_order);
+           die;
 
             if (isset($warehouse_order->GetOrderShipmentInfoResult->OrderShipmentInfo)) {
                 $warehouse_order = $warehouse_order->GetOrderShipmentInfoResult->OrderShipmentInfo;
