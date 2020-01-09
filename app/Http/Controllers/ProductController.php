9bc020cf070f624495f8026d4bf03c298db4a9c1
sunny <sunny_kumar@rvtechnologies.co.in>
2020-01-09 16:43:33 +0530

    data 4

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -178,17 +178,19 @@ class ProductController extends Controller {
 	                    if ($item_value->sku != "")
 	                        $items->appendChild($dom->createElement('Article', $item_value->sku));
 
-	                    if($item_value->title != 'Default Title'){
-	                    // echo "not working";	
-	                    // echo $item_value->title;
-	                    // echo "<br>";
-	                    $items->appendChild($dom->createElement('Title', htmlspecialchars($product->title.':'.$item_value->title)));
-	                    }else{
-	                    // echo "working";
-	                    // echo $product->title; 
-	                    // echo "<br>";	
-	                    $items->appendChild($dom->createElement('Title', htmlspecialchars($product->title)));	
-	                    }
+                        $items->appendChild($dom->createElement('Title', htmlspecialchars($product->title)));
+
+	                    // if($item_value->title != 'Default Title'){
+	                    // // echo "not working";	
+	                    // // echo $item_value->title;
+	                    // // echo "<br>";
+	                    // $items->appendChild($dom->createElement('Title', htmlspecialchars($product->title.':'.$item_value->title)));
+	                    // }else{
+	                    // // echo "working";
+	                    // // echo $product->title; 
+	                    // // echo "<br>";	
+	                    // $items->appendChild($dom->createElement('Title', htmlspecialchars($product->title)));	
+	                    // }
 	                    if ($item_value->barcode != "")
 	                        $items->appendChild($dom->createElement('Barcode', $item_value->barcode));
 	                    $items->appendChild($dom->createElement('BuyPrice', $item_value->price));
