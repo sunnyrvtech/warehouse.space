401ec5e992b6b4a23625d98bd0f15ee9ec0ac3ea
sunny <sunny_kumar@rvtechnologies.co.in>
2020-01-10 16:34:48 +0530

    commit 19

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -98,7 +98,11 @@ class ProductController extends Controller {
                         $item_array = (object) array();
                         $item_array->ProductID = $item_value->id;
                         $item_array->Article = $item_value->sku;
-                        $item_array->Title = htmlspecialchars($request->title.'-'.$item_value->title);
+                        if($item_value->title == 'Default Title'){
+                            $item_array->Title = htmlspecialchars($request->title);
+                        }else{
+                            $item_array->Title = htmlspecialchars($request->title.'-'.$item_value->title);
+                        }    
                         $item_array->Barcode = $item_value->barcode;
                         $item_array->Description = htmlspecialchars(strip_tags($request->body_html));
 //                    $item_array->ErpTimeStamp = date('Y-m-d-H:i');
