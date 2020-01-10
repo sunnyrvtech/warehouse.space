d603cf996512624117e3e20d1e3b631507904e8a
sunny <sunny_kumar@rvtechnologies.co.in>
2020-01-10 15:03:26 +0530

    commit 16

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -172,9 +172,11 @@ class ProductController extends Controller {
 	                    $items = $dom->createElement('MaterialArticle');
 	                    $items->appendChild($dom->createElement('AccountKey', $this->_accountKey));
 	                    $items->appendChild($dom->createElement('ProductID', $item_value->id));
+
+                        $items->appendChild($dom->createElement('Title', htmlspecialchars($product->title)));
 	                    if ($item_value->sku != "")
 	                         $items->appendChild($dom->createElement('Article', htmlspecialchars($item_value->sku)));
-                         $items->appendChild($dom->createElement('Title', htmlspecialchars($product->title)));
+                         
                         //echo htmlspecialchars($product->title.'-'.$item_value->title).'<br>';
 	                    // if($item_value->title != 'Default Title'){
 	                    // // echo "not working";	
